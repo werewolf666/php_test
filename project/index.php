4f5e43ae16e06e501ffd26b069c95071586f0e1a
werewolf666 <1031484669@qq.com>
2018-12-28 18:52:23 +0800

    增加首页index.php

---
+++ project/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gatsby
+ * Date: 18-12-28
+ * Time: 下午3:35
+ */
+
+//首页,加载模板消息
+
+include_once '/var/www/html/test/project/public/public.php';
+
+include_once '/var/www/html/test/templates/index.html';
--- project/login.php
+++ project/login.php
@@ -5,8 +5,9 @@
  * Date: 18-12-27
  * Time: 下午4:06
  */
-header('content-type:text/html;charset=utf-8');
+//header('content-type:text/html;charset=utf-8');
 
+include_once '/var/www/html/test/project/public/public.php';
 //处理用户登录的所有请求
 //判断用户提交信息
 if(isset($_POST['submit'])){
@@ -15,7 +16,7 @@ if(isset($_POST['submit'])){
 
     //接受用户数据
 
-    $usename=trim($_POST['usename']);
+    $username=trim($_POST['usename']);
     $password=trim($_POST['password']);
 
     //合法性验证
@@ -24,13 +25,27 @@ if(isset($_POST['submit'])){
         header('refresh:1,url=login.php');
         echo '用户名密码不能为空';
         exit;
-    }
-    //查询用户名密码是否正确
-    //加载数据库公共文件
-    include_once 'var/www/html/test/project/public/mysql.php';
-    $sql='select * from pr_
+    }else{
+        //查询用户名密码是否正确
+        //加载数据库公共文件
+        $password=md5($password);
+        include_once '/var/www/html/test/project/public/mysql.php';
+    $sql="select * from pr_admin where username='{$username}' and password='{$password}'";
 
-}else{
+    $res=my_error($sql);
+
+    $user=mysqli_fetch_assoc($res);
+
+    if(!$user){
+        echo '用户或者密码错误';
+        header('refresh:1,url=login.php');
+        redirect('login.php','用户名或者密码错误');
+    }
+
+    }
+  
+}else
+{
     //加载登录页面
-    include_once '/var/www/html/test/templates/login.html';
-}
+    include_once "/var/www/html/test/templates/login.html";
+};
